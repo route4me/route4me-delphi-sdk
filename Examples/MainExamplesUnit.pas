11a93753cd4439aa3582ef61cd02d5ac4996c528
Igor Borzenkov <iborzenkov@gmail.com>
2016-11-10 21:34:41 +0800

    ShareRoute sample

--- Examples/MainExamplesUnit.pas
+++ Examples/MainExamplesUnit.pas
@@ -72,7 +72,7 @@ begin
         end;
 
         if (RouteSingleDriverRoute10Stops <> nil) then
-          Examples.ShareRoute(RouteSingleDriverRoute10Stops.RouteId, 'ggora@gmail.com');
+          Examples.ShareRoute(RouteSingleDriverRoute10Stops.RouteId, 'oooooo@gmail.com');
 
         DestinationIds := Examples.AddRouteDestinations(RouteId_SingleDriverRoute10Stops);
         if (Length(DestinationIds) > 0) then
